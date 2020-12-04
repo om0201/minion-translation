937041f8c9ceafde433e76d56557874e2131db97
Omang Morekar <[omangmorekar68@gmail.com]>
2020-12-04 18:52:51 +0530

    updated minion api

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const buttonTranslate = document.querySelector("#btn-translate");
 const input = document.querySelector("#ip")
 const output = document.querySelector("#op")
 
+const url = 'https://api.funtranslations.com/translate/minion.json'
+
 function translate() {
-  output.innerText = input.value;
+
+  var textInput = input.value
+
+  function transUrl(textInput) {
+    return url + '?'+ 'text=' + textInput
+  }
+
+  fetch(transUrl(textInput))
+  .then(resp => resp.json())
+  .then(json => {
+    output.innerText = json.contents.translated
+  })
+
 }
 
 buttonTranslate.addEventListener('click', translate)
